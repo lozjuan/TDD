98d47e7719ad694e9b3dbbbd74c12ea79d36f464
lozjuan <lozjuan@gmail.com>
2017-02-01 12:56:30 +0100

    counting word ocurrences in a phrase

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class WordCount {
+
+    public WordCount() {
+        super();
+    }
+
+    public Map<String, Integer> phrase(String s){
+        Map<String, Integer> WordCount = new HashMap<>();
+        List<String> words = new ArrayList<>(Arrays.asList(s.replaceAll("[\\W]", " ").toLowerCase().split("\\s+")));
+        for (String word : words) {
+            Integer count = WordCount.get(word);
+            WordCount.put(word, count == null ? 1 : count + 1);
+        }
+        return WordCount;
+    }
 }
+
--- word-count/src/test/java/WordCountTest.java
+++ word-count/src/test/java/WordCountTest.java
@@ -32,7 +32,6 @@ public class WordCountTest {
         );
     }
 
-    @Ignore
     @Test
     public void countOneOfEach() {
         expectedWordCount.put("one", 1);
@@ -45,7 +44,6 @@ public class WordCountTest {
         );
     }
 
-    @Ignore
     @Test
     public void countMultipleOccurences() {
         expectedWordCount.put("one", 1);
@@ -60,7 +58,6 @@ public class WordCountTest {
         );
     }
 
-    @Ignore
     @Test
     public void ignorePunctuation() {
         expectedWordCount.put("car", 1);
@@ -76,7 +73,6 @@ public class WordCountTest {
 
     }
 
-    @Ignore
     @Test
     public void includeNumbers() {
         expectedWordCount.put("testing", 2);
@@ -89,7 +85,6 @@ public class WordCountTest {
         );
     }
 
-    @Ignore
     @Test
     public void normalizeCase() {
         expectedWordCount.put("go", 3);
